8b82ee8fc0c252e368101a3cbcbcf7e6cc176b3b
sagarbhandari1 <91552488+sagarbhandari1@users.noreply.github.com>
2025-10-12 08:10:58 +0000

    daat

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 14423
+let accountEmail = "bhandarisagar6789@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountstate;
+
+//accountId =2 //not allowed
+console.log(accountId);
+ accountEmail = "mavav.com"
+ accountCity="Bengalury"
+ accountPassword="212121"
+
+ console.table([accountEmail,accountId,accountPassword,accountCity,accountstate])
+ /*
+prefer not to use var
+because of issue in block scope and function scope
+ */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict " ;// treat all js code as newer version
+
+//alert("hello") 
+
+//alert(3+3)// we are using node js ,not browser
+
+console.log(3+
+    
+    3); //code readability should be high
+
+    let name = "sagar"
+    let age = 18
+    let isLoggedIn =false
+//types of datatyes
+    //number = 2 to the power 53
+    //bigint
+    //string = ""
+    //boolean = true or false
+    //null = standalone value
+    //undefined = let state // varible define krdia hai pr value define nhi kri 
+    //symbol /uniqueness find krne k liye zaruri hota hai
+
+    //object
+
+console.log(typeof "sagar");
+console.log(typeof age);
+
+console.log(typeof null);//output is object
+console.log(typeof undefined);//output is undefined
+
+
